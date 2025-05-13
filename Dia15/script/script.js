818754e72271e227414867fbe1b7b76a7a801314
Juan-771 <abrilromanj@gmail.com>
2025-05-13 09:14:04 -0500

    feat: :alien: modification Day15 script

--- Dia15/script/script.js
+++ Dia15/script/script.js
@@ -1,4 +1,15 @@
+const clientId = 'i3iVyVhYs8Fe7sCm08ILfSwWANBOvZYXNYKvz1pLzEsk74ZD8R';
+const clientSecret = 'SZpQ6DcjYFSDew7IV0LszaAXmOvrOqhDC2N6JHtV';
 
-icon search-icon.addEventListener("click", () => {
-    esto.style.display = "block"
-  })
+async function getAccesToken(){
+  const response = await fetch('https://api.petfinder.com/v2/oauth2/token', {
+    method:'POST',
+    headers:{
+      'Content-Type':'application/x-www-form-urlencoded'
+    },
+    body: `gran_type=client_credentials&client_id=${clientId}&client_secret${clientSecret}`
+  });
+  const data = await response.json();
+  return data.access_token;
+  
+}
